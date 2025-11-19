103e59a1c20a4b2f323f556688ab37b15e5b30c2
Mayerly Cardenas <nubia_mcardenas@soy.sena.edu.co>
2025-11-19 06:49:39 -0500

    Practicas y desarrollo ejercicio practico

--- members/models.py
+++ members/models.py
@@ -5,4 +5,7 @@ class Member(models.Model):
   lastname = models.CharField(max_length=255)
   phone = models.IntegerField(null=True)
   joined_date = models.DateField(null=True)
+  email = models.EmailField(null=True)
+  address = models.CharField(max_length=255, null=True)     
+  city = models.CharField(max_length=255, null=True)
   
