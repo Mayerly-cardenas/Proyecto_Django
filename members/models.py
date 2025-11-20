0ff7010c1df492374fe25ab3aba78324ce8f9421
Mayerly Cardenas <nubia_mcardenas@soy.sena.edu.co>
2025-11-20 08:59:32 -0500

    Sitio Web con Django

--- members/models.py
+++ members/models.py
@@ -1,3 +1,4 @@
+
 from django.db import models
 
 class Member(models.Model):
@@ -8,4 +9,9 @@ class Member(models.Model):
   email = models.EmailField(null=True)
   address = models.CharField(max_length=255, null=True)     
   city = models.CharField(max_length=255, null=True)
-  
+
+    # Nuevo campo de imagen
+image = models.ImageField(upload_to='member_images/', blank=True, null=True)
+
+def __str__(self):
+        return f"{self.firstname} {self.lastname}"
