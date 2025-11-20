bc6d32cd41a18991e0892aba6776f5129b00bb0a
Mayerly Cardenas <nubia_mcardenas@soy.sena.edu.co>
2025-11-20 11:05:00 -0500

    Panel - Admin DjangoY CRUD - Django ya tenia unos cambios

--- my_tennis_club/settings.py
+++ my_tennis_club/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-=rhohmzcbywg+zh$_p*9mm=8oc#+1t^^#zrt&@x9ulqa$n2u@)"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
